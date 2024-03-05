09795882e20a7d87d0679e1ff7dc40de29d26070
Sullivan18 <andre.sullivan18@hotmail.com>
2024-03-05 12:02:21 -0300

    changes

--- scripts.js
+++ scripts.js
@@ -1,25 +1,3 @@
-function calcular() {
-    var num1 = document.getElementById("num1").value;
-    var num2 = document.getElementById("num2").value;
-    var operacao = document.getElementById("operacao").value;
-    var resultado = 0;
-
-    
-    if ( operacao === "+") {
-      resultado = parseInt(num1) + parseInt(num2);
-    } else if ( operacao === "-") {
-      resultado = parseint(num1) - parseint(num2);
-    }
-    else if ( operacao === "*") {
-      resultado = parseInt(num1) * parseInt(num2);
-    }
-    else if ( operacao === "/") {
-      resultado = parseInt(num1) / parseInt(num2);
-    }
-
-
-    document.getElementById("resultado").innerHTML = resultado;
-  }
-
-
-  
+alert("Olá, mundo!");
+confirm('Está gostando do cogido?')
+prompt('Feedback')
